6cc3985dbfc0fafbbd478da40be817c98fcf85ad
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-11-21 10:28:57 -0300

    Cadastrando user

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma'
+
 
 interface UserRequest{
     name: string;
@@ -8,9 +10,36 @@ interface UserRequest{
 class CreateUserService{
     async execute({name, email, password}: UserRequest){
 
-        console.log(name);
+        //verificar se ele enviou um email
+        if(!email){
+            throw new Error("Email incorrect")
+        }
 
-        return { name: name }
+        //Verificar se esse email já está cadastrado na plataforma
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email:email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error("User already exists")
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password,
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+
+        return user;
     }
 }
 
