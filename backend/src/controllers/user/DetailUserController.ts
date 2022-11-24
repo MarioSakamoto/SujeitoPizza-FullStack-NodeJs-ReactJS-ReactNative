e2566b8c1aac19e446a75d8d33320ec4257aca48
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-11-24 01:51:34 -0300

    Detalhes do usuário

--- backend/src/controllers/user/DetailUserController.ts
+++ backend/src/controllers/user/DetailUserController.ts
@@ -4,13 +4,17 @@ import { DetailUserService } from '../../services/DetailUserService'
 class DetailuserController{
   async handle(req: Request, res: Response){
 
+    const user_id = req.user_id;
+
+    console.log("ID DO USER: ", user_id)
+
     const detailUserService = new DetailUserService();
 
-    const user = await detailUserService.execute();
+    const user = await detailUserService.execute(user_id);
 
     return res.json(user);
 
   }
 }
 
-export { DetailuserController  }
+export { DetailuserController }
