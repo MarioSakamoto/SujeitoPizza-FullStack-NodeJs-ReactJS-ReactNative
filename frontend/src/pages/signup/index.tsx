03332b3ac6ea2e6e26fbace8106dd1c6775bc6d6
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-11-30 15:29:41 -0300

    Criando pagina de cadastro

---
+++ frontend/src/pages/signup/index.tsx
@@ -0,0 +1,51 @@
+import Head from 'next/head'
+import Image from 'next/image';
+import styles from '../../../styles/home.module.scss';
+
+import logoImg from '../../../public/logo.svg';
+
+import { Input } from '../../components/ui/Input'
+import { Button } from '../../components/ui/Button'
+
+import Link from 'next/link';
+
+export default function SignUp() {
+  return (
+    <>
+    <Head>
+      <title> Faça seu cadastro agora! </title>
+    </Head>
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Sujeito Pizzaria"/>
+
+      <div className={styles.login}>
+
+        <h1>Criando sua conta</h1>
+
+        <form>
+          <Input placeholder="Digite seu nome"
+          type="text" />
+
+          <Input placeholder="Digite seu email"
+          type="password" />
+
+          <Input placeholder="Digite seu senha"
+          type="text" />
+
+          <Button
+           type="submit"
+           loading={false}
+          >
+            Cadastrar
+            </Button>
+        </form>
+
+        <Link legacyBehavior href="/">
+          <a className={styles.text}>Já possui uma conta?  Faça o login!</a>
+        </Link>
+        
+      </div>
+    </div>
+      </>
+  )
+}
