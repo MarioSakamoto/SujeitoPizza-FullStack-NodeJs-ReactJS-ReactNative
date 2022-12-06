d832b071c2a7cbb63a52c9c2751f8aab80c6388c
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-12-06 00:17:36 -0300

    Buscando categorias

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -7,12 +7,24 @@ import { canSSRAuth } from '../../utils/canSSRAuth'
 
 import { FiUpload } from 'react-icons/fi';
 
+import { setupAPIClient } from '../../services/api';
 
+type ItemProps = {
+    id: string;
+    name: string;
+  }
+  
+  interface CategoryProps{
+    categoryList: ItemProps[];
+  }
 
-export default function Product(){
+export default function Product({ categoryList }: CategoryProps){
 
     const [avatarUrl, setAvatarUrl] = useState ('');
     const [imageAvatar, setImageAvatar] = useState (null);
+
+    const [categories, setCategories] = useState(categoryList || [])
+    const [categorySelected, setCategorySelected] = useState(0)
 
     function handleFile(e: ChangeEvent<HTMLInputElement>){
 
@@ -34,6 +46,15 @@ export default function Product(){
 
     }
 
+    //quando vc seleciona uma nova categoria na lista
+    function handleChangeCategory(event){
+        //console.log("POSICAO DA CATEGORIA SELECIONADA ", event.target.value)
+        //console.log('Categoria selecionada', categories[event.target.value])
+
+        setCategorySelected(event.target.value)
+    }
+
+
   return(
     <>
       <Head>
@@ -49,12 +70,12 @@ export default function Product(){
 
           <form className={styles.form}>
 
-<label className={styles.labelAvatar}>
-  <span>
-    <FiUpload size={30} color="#FFF" />
-  </span>
+            <label className={styles.labelAvatar}>
+                <span>
+                    <FiUpload size={30} color="#FFF" />
+                </span>
 
-  <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
+                <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
 
   {avatarUrl && (     
       <img 
@@ -69,13 +90,14 @@ export default function Product(){
 </label>
 
 
-            <select>
-              <option>
-                Bebida
-              </option>
-              <option>
-                Pizzas
-              </option>
+            <select value={categorySelected} onChange={handleChangeCategory} >
+                {categories.map( (item, index) => {
+                  return(
+                    <option key={item.id} value={index}>
+                      {item.name}
+                    </option>
+                  )
+                })}
             </select>
 
             <input 
@@ -109,8 +131,14 @@ export default function Product(){
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+    const apliClient = setupAPIClient(ctx)
 
-  return {
-    props: {}
-  }
+    const response = await apliClient.get('/category');
+    //console.log(response.data);
+
+    return {
+    props: {
+        categoryList: response.data
+        }
+    }
 })
