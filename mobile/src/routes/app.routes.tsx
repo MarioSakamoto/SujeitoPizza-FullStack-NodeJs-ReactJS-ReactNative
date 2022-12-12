589dc70a93c7cae9070d5a5fe05d8e16572bf722
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-12-12 14:39:26 -0300

    Abrindo mesa

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -2,15 +2,31 @@ import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import Dashboard from '../pages/SignIn/Dashboard';
+import Order from '../pages/Order';
 
-const Stack = createNativeStackNavigator();
+export type StackPramsList = {
+    Dashboard: undefined;
+    Order: {
+        number: number | string;
+        order_id: string;
+    };
+};
+
+const Stack = createNativeStackNavigator<StackPramsList>();
 
 function AppRoutes(){
     return(
         <Stack.Navigator>
-            <Stack.Screen name="Dashboard" 
+            <Stack.Screen 
+            name="Dashboard" 
             component={Dashboard} 
             options={{headerShown: false}}
+            />
+
+            <Stack.Screen
+                name="Order"
+                component={Order}
+                options={{ headerShown: false}}
             />
         </Stack.Navigator>
     )
