e7182911add668cb0d32fc5a3cb3d3144d4395b8
Mario Sakamoto <mariosakamoto.dev@gmail.com>
2022-12-13 19:35:11 -0300

    Adicionando itens

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -99,8 +99,23 @@ export default function Order(){
       setProductSelected(item);
     }
 
+    //add um produto nessa mesa
     async function handleAdd() {
-      console.log("clicou")
+      const response = await api.post('/order/add', {
+        order_id: route.params?.order_id,
+        product_id: productSelected?.id,
+        amount: Number(amount)
+      })
+  
+      let data = {
+        id: response.data.id,
+        product_id: productSelected?.id as string,
+        name: productSelected?.name as string,
+        amount: amount
+      }
+  
+      setItems(oldArray => [...oldArray, data])
+  
     }
 
 
@@ -109,9 +124,11 @@ export default function Order(){
            
            <View style={styles.header}>
             <Text style={styles.title}>Mesa {route.params.number}</Text>
-            <TouchableOpacity onPress={handleCloseOrder}>
-                <Feather name="trash-2" size={28} color="#FF3F4b" />
+           {items.length ===0 && (
+             <TouchableOpacity onPress={handleCloseOrder}>
+              <Feather name="trash-2" size={28} color="#FF3F4b" />
              </TouchableOpacity>
+           )}
            </View>
 
            {category.length !== 0 && (
